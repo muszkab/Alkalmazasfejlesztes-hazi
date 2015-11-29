8fd90ce8b2965a649858030625a340c56ac1e7e8
MuszkaB <m1musbal@gmail.com>
2015-11-29 21:07:07 +0100

    main próbálgatás

--- RobotDiagnosticApp/main.cpp
+++ RobotDiagnosticApp/main.cpp
@@ -1,13 +1,19 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+//#include <QDebug>
+#include "szimulator.h"
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    Q_UNUSED(argc);
+    Q_UNUSED(argv);
+
+    Szimulator app(3333);
+    app.start(1);
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
-    return app.exec();
+    return 0;
 }
 
